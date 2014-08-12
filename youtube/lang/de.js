e04a2acbbf00595cf8ec77d10068c9248196465b
Malte <mdoelker@o2online.de>
2014-08-12 15:46:01 +0200

    Updated English and German language file.

--- youtube/lang/de.js
+++ youtube/lang/de.js
@@ -1,14 +1,15 @@
 CKEDITOR.plugins.setLang('youtube', 'de', {
-	button : 'Youtube Video einbinden',
-	title : 'Youtube Video einbinden',
+	button : 'YouTube Video einbinden',
+	title : 'YouTube Video einbinden',
 	txtEmbed : 'Embed Code hier einfügen',
-	txtUrl : 'Youtube Video URL hier einfügen',
+	txtUrl : 'YouTube Video URL hier einfügen',
 	txtWidth : 'Breite',
 	txtHeight : 'Höhe',
-	chkRelated : 'Video Vorschläge am Video Ende einblenden',
+	txtResponsive : 'Automatische Größe (ignoriert Breite und Höhe)',
+	chkRelated : 'Vorschläge am Ende des Videos einblenden',
 	txtStartAt : 'Start bei Position (ss oder mm:ss oder hh:mm:ss)',
-	chkPrivacy : 'Aktiviere die erweiterte Privatsphäre',
-	chkOlderCode : 'Benutzer alten Embed Code',
+	chkPrivacy : 'Erweiterten Datenschutzmodus aktivieren',
+	chkOlderCode : 'Benutze alten Embed Code',
 	chkAutoplay : 'Autoplay',
 	noCode : 'Sie müssen einen Embed Code oder URL angeben',
 	invalidEmbed : 'Der angegebene Embed Code scheint nicht gültig zu sein.',
@@ -19,6 +20,5 @@ CKEDITOR.plugins.setLang('youtube', 'de', {
 	noHeight : 'Geben Sie eine Höhe an',
 	invalidHeight : 'Geben Sie eine gültige Höhe an',
 	invalidTime : 'Geben Sie eine gültige Startzeit an',
-	chkSourceLink : 'Include link to YouTube',
-	txtResponsive : 'Responsive video'
+	chkSourceLink : 'Link zu YouTube anzeigen'
 });
